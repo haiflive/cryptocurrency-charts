4847d2c97be2c191a8a89c75602cc2b45dd93eeb
haiflive <jak_67@mail.ru>
2017-06-11 21:45:22 +1000

    Add icons; add timer and throttle; add grouping;

--- src/app/poloniex-api.service.ts
+++ src/app/poloniex-api.service.ts
@@ -25,14 +25,20 @@ export class PoloniexApiService {
     {data: [28, 48, 40, 19, 96, 27, 100], label: 'Asks'}
   ];
   
+  private testResponseData:string = `{"asks":[["0.09383489",0.8011014],["0.09387000",1.0000149],["0.09388002",10.65189376],["0.09388003",13.60968098],["0.09388004",6.94837911],["0.09390000",3],["0.09394259",0.17465731],["0.09395366",0.11398516],["0.09399902",119.085],["0.09399924",1.00046646]],"bids":[["0.09380000",7.59922072],["0.09379865",4.61038398],["0.09379546",2.15857887],["0.09377000",27.98767601],["0.09373510",5.88192837],["0.09371668",0.28030449],["0.09365000",8.54700865],["0.09360515",113.64387484],["0.09360514",85.079],["0.09360513",14.57627893]],"isFrozen":"0","seq":356717781}`;
+  private debugMode:boolean = false;
+  
   private lineChartData:Array<any> = [
     [65, 59, 80],
     [28, 48, 40]
   ];
   
-  public getMarketCyrrency(cyrrency_code:string, chart_depth:number) : Promise<any> {
-    // return Promise.resolve(this.radarChartData);
-    const url = `https://poloniex.com/public?command=returnOrderBook&currencyPair=`+cyrrency_code+`&depth=`+chart_depth;
+  public getMarketCyrrency(cyrrency_code:string, chart_depth:number, cyrrency_initial_code:string = 'BTC') : Promise<any> {
+    if(this.debugMode) {
+        return Promise.resolve(JSON.parse(this.testResponseData));
+    }
+    
+    const url = `https://poloniex.com/public?command=returnOrderBook&currencyPair=`+cyrrency_initial_code+`_`+cyrrency_code+`&depth=`+chart_depth;
     
     return this.http.get(url)
             .toPromise()
--- src/app/radar-chart-currency/radar-chart-currency.component.ts
+++ src/app/radar-chart-currency/radar-chart-currency.component.ts
@@ -1,5 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { PoloniexApiService } from '../poloniex-api.service';
+import { Observable } from 'rxjs/Rx';
+import * as _ from "lodash";
+
+const COINS = [
+  {
+    id: 'ETH',
+    name: 'Ethereum',
+    img: 'ethereum'
+  }, {
+    id: 'XRP',
+    name: 'Ripple',
+    img: 'ripple'
+  }, {
+    id: 'XEM',
+    name: 'NEM',
+    img: 'nem'
+  }, {
+    id: 'XMR',
+    name: 'Monero',
+    img: 'monero'
+  }, {
+    id: 'BCN',
+    name: 'Bytecoin',
+    img: 'bytecoin-bcn'
+  }, {
+    id: 'DGB',
+    name: 'DigiByte',
+    img: 'digibyte'
+  }, {
+    id: 'ZEC',
+    name: 'Zcash',
+    img: 'zcash'
+  }, {
+    id: 'LSK',
+    name: 'Lisk',
+    img: 'lisk'
+  }, {
+    id: 'SC',
+    name: 'Siacoin',
+    img: 'siacoin'
+  }, {
+    id: 'LBC',
+    name: 'LBRY Credits',
+    img: 'library-credit'
+  }
+];
 
 @Component({
   selector: 'app-radar-chart-currency',
@@ -7,13 +53,24 @@ import { PoloniexApiService } from '../poloniex-api.service';
   styleUrls: ['./radar-chart-currency.component.css']
 })
 export class RadarChartCurrencyComponent implements OnInit {
-
+  
   constructor(private _poloniexService:PoloniexApiService) {
-    this.value = this.items[0];
-    this.refreshChartData();
   }
 
   ngOnInit() {
+    console.log('lodash version:', _.VERSION);
+    
+    COINS.forEach((coin:{id: string, name:string, img:string}) => {
+      this.items_select.push({
+        id: coin.id,
+        text: `<img src='assets/images/currencies/${coin.img}.png' /> ${coin.name}`
+      });
+    });
+    
+    this.value_select = this.items_select[0];
+    
+    this.refreshChartData();
+    this.setupRefresher();
   }
   
   // lineChart
@@ -75,35 +132,8 @@ export class RadarChartCurrencyComponent implements OnInit {
   ];
   
   // Select
-  public items:Array<any> = [
-    {
-      id: 'BTC_ETH',
-      text: 'Ethereum',
-    }, {
-      id: 'BTC_XRP',
-      text: 'Ripple',
-    }, {
-      id: 'BTC_XEM',
-      text: 'NEM',
-    }, {
-      id: 'BTC_XMR',
-      text: 'Monero',
-    }, {
-      id: 'BTC_BCN',
-      text: 'Bytecoin',
-    }, {
-      id: 'BTC_DGB',
-      text: 'DigiByte',
-    }, {
-      id: 'BTC_ZEC',
-      text: 'Zcash',
-    }, {
-      id: 'BTC_LSK',
-      text: 'Lisk',
-    }
-  ];
-  
-  private value:any = {};
+  public items_select:Array<any> = [];
+  private value_select:any = {};
  
   public selected(value:any):void {
     console.log('Selected value is: ', value);
@@ -118,15 +148,15 @@ export class RadarChartCurrencyComponent implements OnInit {
   }
  
   public refreshValue(value:any):void {
-    this.value = value;
+    this.value_select = value;
     this.refreshChartData();
   }
   // --
   
-  private _chartDepth:number = 10;
+  private _chartDepth:number = 50;
   private _chartThrottle:number = -1; // default disabled
-  public _chartGrouping:boolean = false;
-  private _chartGroupingDecimals:number = 6; // default 6 decimals
+  public _chartGrouping:boolean = true;
+  private _chartGroupingDecimals:number = 9; // default 9 decimals
  
   private get chartDepthV():number {
     return this._chartDepth;
@@ -143,6 +173,7 @@ export class RadarChartCurrencyComponent implements OnInit {
  
   private set chartThrottleV(value:number) {
     this._chartThrottle = value;
+    this.setupRefresher();
   }
   
   private get chartGroupingV():string {
@@ -151,6 +182,7 @@ export class RadarChartCurrencyComponent implements OnInit {
  
   private set chartGroupingV(value:string) {
     this._chartGrouping = value === '1';
+    this.refreshChartData();
   }
   
   private get chartGroupingDecimalsV():number {
@@ -159,6 +191,7 @@ export class RadarChartCurrencyComponent implements OnInit {
  
   private set chartGroupingDecimalsV(value:number) {
     this._chartGroupingDecimals = value;
+    this.refreshChartData();
   }
   
   // --
@@ -174,45 +207,123 @@ export class RadarChartCurrencyComponent implements OnInit {
   }
 
   private refreshChartData():void {
-    this._poloniexService.getMarketDepth().then((charts) => this.radarChartData = charts);
+    // this._poloniexService.getMarketDepth().then((charts) => this.radarChartData = charts);
     
-    this._poloniexService.getMarketCyrrency(this.value.id, this._chartDepth).then((data) => {
-        let chart_data_asks: Array<number> = [];
-        let chart_data_bids: Array<number> = [];
-        let chart_data_asks_depth: Array<number> = [];
-        let chart_data_bids_depth: Array<number> = [];
-        let chart_labels: Array<string> = [];
+    this._poloniexService.getMarketCyrrency(this.value_select.id, this._chartDepth).then((data) => {
+        var result : any;
         
-        
-        for(let i = 0, total:number = 0; i < data.bids.length; i++) {
-            total += data.bids[i][1];
-            chart_data_asks.unshift(0);
-            chart_data_asks_depth.unshift(0);
-            chart_data_bids.unshift(data.bids[i][1]);
-            chart_data_bids_depth.unshift(total);
-            chart_labels.unshift(data.bids[i][0]+' BTC');
+        if(this._chartGrouping) {
+          result = this.prepareDepthDataGrouped(data, this._chartGroupingDecimals);
+        } else {
+          result = this.prepareDepthDataLinear(data);
         }
         
-        for(let i = 0, total:number = 0; i < data.asks.length; i++) {
-            total += data.asks[i][1];
-            chart_data_asks.push(data.asks[i][1]);
-            chart_data_asks_depth.push(total);
-            chart_data_bids.push(0);
-            chart_data_bids_depth.push(0);
-            chart_labels.push(data.asks[i][0]+' BTC');
-        }
-        
-        this.lineChartLabels = chart_labels;
+        this.lineChartLabels = result.chart_labels;
         
         setTimeout(() => {
-          this.lineChartData = [
-                {data: chart_data_asks, label: 'Asks'},
-                {data: chart_data_bids, label: 'Bids'},
-                {data: chart_data_asks_depth, label: 'Asks Depth'},
-                {data: chart_data_bids_depth, label: 'Bids Depth'}
-            ];
+          this.lineChartData = result.chart_data;
         }, 50);
         
     });
   }
+  
+  private prepareDepthDataLinear(data:any) : any {
+      let chart_data_asks: Array<number> = [];
+      let chart_data_bids: Array<number> = [];
+      let chart_data_asks_depth: Array<number> = [];
+      let chart_data_bids_depth: Array<number> = [];
+      let chart_labels: Array<string> = [];
+      
+      
+      for(let i = 0, total:number = 0; i < data.bids.length; i++) {
+          total += data.bids[i][1];
+          chart_data_asks.unshift(0);
+          chart_data_asks_depth.unshift(0);
+          chart_data_bids.unshift(data.bids[i][1]);
+          chart_data_bids_depth.unshift(total);
+          chart_labels.unshift(data.bids[i][0]+' BTC');
+      }
+      
+      for(let i = 0, total:number = 0; i < data.asks.length; i++) {
+          total += data.asks[i][1];
+          chart_data_asks.push(data.asks[i][1]);
+          chart_data_asks_depth.push(total);
+          chart_data_bids.push(0);
+          chart_data_bids_depth.push(0);
+          chart_labels.push(data.asks[i][0]+' BTC');
+      }
+        
+      return {
+          chart_labels: chart_labels,
+          chart_data: [
+                {data: chart_data_asks, label: 'Asks'},
+                {data: chart_data_bids, label: 'Bids'},
+                {data: chart_data_asks_depth, label: 'Asks Depth'},
+                {data: chart_data_bids_depth, label: 'Bids Depth'}
+            ]
+      }
+  }
+  
+  private prepareDepthDataGrouped(data:any, deciamls:number) : any {
+    data.asks = this.groupChartPoints(data.asks, deciamls);
+    data.bids = this.groupChartPoints(data.bids, deciamls);
+    
+    return this.prepareDepthDataLinear(data);
+  }
+  
+  private groupChartPoints(points:Array<number>, round_dec:number) : Array<number> {
+    if(round_dec > 17)
+      round_dec = 17;
+    
+    let result: Array<number> = [];
+    let chart_pint_first = points[0]; // first point
+    chart_pint_first[0] = (+chart_pint_first[0]).toFixed(round_dec);// round price
+    result.push(chart_pint_first);
+    
+    for(let i = 1; i< points.length; i++) {
+      let chart_pint = points[i];
+      chart_pint[0] = (+chart_pint[0]).toFixed(round_dec);// round price
+      
+      let last_point_index = result.length - 1;
+      
+      if( (+result[last_point_index][0]).toFixed(round_dec) == (+chart_pint[0]).toFixed(round_dec) ) {
+        result[last_point_index][1] = +result[last_point_index][1] + +chart_pint[1]; // sum count
+      } else {
+        result.push(chart_pint); // new point
+      }
+    }
+    
+    return result;
+  }
+  
+  
+  /**
+   *  refresh chart Throttle
+   */
+  private observableThrottle;
+  private observableThrottleSubscribe;
+  
+  private setupRefresher() : void {
+    if(!this._chartThrottle || this._chartThrottle < 0)
+      return;
+    
+    if(!this.observableThrottle) { // first call
+      this.observableThrottle = Observable.timer(500)
+            .switchMap((ev) => Observable.interval(+this._chartThrottle * 1000));
+            // .catch(this.handleError)
+    } else {
+      this.observableThrottle.switchMap((ev) => Observable.interval(+this._chartThrottle * 1000));
+    }
+    
+    if(this.observableThrottleSubscribe) {
+      this.observableThrottleSubscribe.unsubscribe();
+    }
+    
+    this.observableThrottleSubscribe = this.observableThrottle.subscribe(() => {
+          this.refreshChartData();
+      })
+    
+    return;
+  }
+  
 }
